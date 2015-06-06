f8e9d39273a22b6971e1b0a5ad183503c372f357
Rob Monk <robert.monk85@gmail.com>
2015-06-06 17:14:34 -0400

    Shiny new map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,11 +18,11 @@
 $(function(){ $(document).foundation();
   var map = L.map('map').setView([42.351776, -71.061371], 14);
   accessToken = 'pk.eyJ1IjoidmlydHVhbC1tb25rIiwiYSI6Ijc2MkxoLVUifQ.0QGDsafsrp25jF3eN6eUeQ';
-  L.tileLayer('https://{s}.tiles.mapbox.com/v4/virtual-monk.ln2l3lfc/{z}/{x}/{y}.png?access_token=' + accessToken, {
+  L.tileLayer('https://api.tiles.mapbox.com/v4/virtual-monk.ln2km151/{z}/{x}/{y}.png?access_token=' + accessToken, {
       attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
   }).addTo(map);
 
-  $.get( '/events.json', function( data ) {
+  $.get( '/recommendation.json', function( data ) {
     for (i=0; i<data.length; i++) {
       var coordinatesArray =  [data[i].latitude, data[i].longitude];
       var marker = L.marker([coordinatesArray[0],coordinatesArray[1]]);

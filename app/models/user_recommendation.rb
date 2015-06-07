a43f30017116eacb2d067d16d8fbf20d0fb14dfa
Rob Monk <robert.monk85@gmail.com>
2015-06-06 21:25:12 -0400

    added map and form

--- app/models/user_recommendation.rb
+++ app/models/user_recommendation.rb
@@ -1,2 +1,5 @@
 class UserRecommendation < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :recommendation
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   resources :recommendations
+  resources :user_recommendations
   root 'homes#index'
-<<<<<<< HEAD
   get '/map', to:'recommendations#map'
-
-  devise_for :users
-
-=======
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
->>>>>>> master
+
 end

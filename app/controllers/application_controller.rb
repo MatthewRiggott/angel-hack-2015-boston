24391758bc807a87a490daa3d08118a3c6a57508
emanoppo <emanoppo@users.noreply.github.com>
2015-06-07 01:39:07 -0400

    Add user log in verification.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :verify_logged_in
+
+  def verify_logged_in
+  	if params[:controller] != "devise/registrations" && params[:controller] != "devise/sessions" && params[:controller] != "omniauth_callbacks"
+       redirect_to new_user_session_path
+    end
+  end
 end

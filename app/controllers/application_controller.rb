17d3ad5e2398ea8b08b0351c3c3f8626dafa149d
Matthew Riggott <Matthew.S.Riggott@gmail.com>
2015-06-07 02:40:34 -0400

    fix login button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
+  before_filter :verify_logged_in
+  # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def verify_logged_in
+    # if we are not logging in or signing up and aren't signed up go to loginpage
+
+    if current_user.nil?
+      if params[:controller] != "devise/registrations" && params[:controller] != "devise/sessions"  && params[:controller] != "omniauth_callbacks"
+        redirect_to new_user_session_path
+      end
+    end
+  end
+  # For APIs, you may want to use :null_session instead.
+
 end
